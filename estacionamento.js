c21eea45fcf501b4ec96a64f1b1b5d48d0907832
EnriqueLindo <54629804+EnriqueLindo@users.noreply.github.com>
2020-10-26 22:21:07 -0300

    Update estacionamento.js

--- estacionamento.js
+++ estacionamento.js
@@ -1,9 +1,9 @@
 var modal = document.getElementById("popup");
 
-var btn = document.getElementById("popup_local");
-
-btn.onclick = function() {
-  modal.style.display = "block";
+function modal_show(lugar){
+    var par = window.document.getElementById("local");
+    par.innerHTML = `Você está em: ${lugar}?`
+    modal.style.display = "block";
 }
 
 function modal_close(){
