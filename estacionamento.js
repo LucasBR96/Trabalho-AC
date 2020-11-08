9e02dc5f7e37809f637659abd3aac5b7c83b06ee
EnriqueLindo <54629804+EnriqueLindo@users.noreply.github.com>
2020-11-08 14:46:11 -0300

    Update estacionamento.js

--- estacionamento.js
+++ estacionamento.js
@@ -9,6 +9,24 @@ function modal_close(md){
     my_modal.style.display = "none";
 }
 
-function teste(){
-    window.alert("ok")
+function vaga_get(){
+    var andar = window.document.getElementById("vaga_andar")
+    var num = window.document.getElementById("vaga_num")
+
+    andar.innerHTML = '<strong>G3</strong>'
+    num.innerHTML = '<strong>32</strong>'
+
+    window.document.getElementById("vaga").style.display = "block"
+
+}
+
+function estacionado(){
+    var btn = window.document.getElementById("btn_estacionado")
+
+    if (btn.innerHTML == 'Estacionado!'){
+        btn.innerHTML = 'Confirmar Saída'
+    }else{
+        btn.innerHTML = 'Estacionado!'
+    }
+
 }
